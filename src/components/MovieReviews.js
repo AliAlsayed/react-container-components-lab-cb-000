28f0f0ece783e60b3dd845000eab003021dbb586
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-18 08:58:20 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,5 +1,14 @@
 // Code MovieReviews Here
 import React from 'react';
-export default class MovieReviews extends React.Component {
+// export default class MovieReviews extends React.Component {
+//   render(){
+//     return (
+//       <div 
+//     )
+//   }
+// }
 
-}
+const MovieReviews = ({headline, byline, link, summary_short}) => (
+  <div></div>
+)
+export default MovieReviews;
